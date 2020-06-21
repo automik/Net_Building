d1802110856bcbf5cfe5c796d34185d6091cb966
automik <55752371+automik@users.noreply.github.com>
2020-06-22 00:04:03 +0500

    YES, IT IS WORKING. God that was hard to debug. But here it is working like a clock (i'm not sure about that, but i think it's pretty good). So now i'll start to work on genetic algorithm.

--- neural_network/neural network.py
+++ neural_network/neural network.py
@@ -5,6 +5,12 @@ np.printoptions()
 
 
 def activation(x):
+    """
+    This function is basic activation function,
+    it is made that way so it will be easy to change
+    :param x: value
+    :return:
+    """
     return 1 / (1 + math.exp(-x))
 
 
@@ -17,44 +23,46 @@ class NeuralNetwork:
 
 
     def build_neural_network(self, *layers):
+        """
+        :param layers: separate layers (number of neurons in each layer
+        :return: built neural network (each layer is values of neurons, biases, weights)
+        """
         if len(layers) < 2:
-            raise(Exception('Neural network need at least two layers'))
+            raise(Exception('Neural network needs at least two layers'))
 
         built_neural_network = []
         for i in range(0, len(layers)):
             random.seed()
             neuron_count = layers[i]
-            biases = np.array([(random.randint(-100, 100)/10) for x in range(0, neuron_count)])
+            biases = np.array([random.uniform(-10, 10) for x in range(0, neuron_count)])
             weights = []
             if i != len(layers) - 1:
                 for j in range(0, layers[i + 1]):
-                    weights.append(np.array([(random.randint(-100, 100)/10) for x in range(0, neuron_count)]))
-            print([np.array([0]*neuron_count, float), biases, np.array(weights)])
-            instant_layer = np.array([np.array([0]*neuron_count, float), biases, np.array(weights)])
-            print(instant_layer)
+                    weights.append(np.array([random.uniform(-10, 10) for x in range(0, neuron_count)]))
+            instant_layer = [np.array([0]*neuron_count, float), biases, np.array(weights)]
             built_neural_network.append(instant_layer)
-        print(built_neural_network, '\n')
-        built_neural_network = np.array(built_neural_network)
         print(built_neural_network, '\n')
         self.neural_network = built_neural_network
 
     def run_neural_network(self, input_values):
-        if self.neural_network is not None:
-            input_values = np.array(input_values)
-            self.neural_network[0][0] = activation(input_values + self.neural_network[0][1])
-            for i in range(0, len(self.neural_network) - 1):
-                instant_layer = []
-                for j in range(0, len(self.neural_network[i][2])):
-                    instant_layer.append(self.neural_network[i][0][j]*self.neural_network[i][2][j]
-                                         + self.neural_network[i+1][1][j])
-                self.neural_network[i + 1][0] = activation(np.array(instant_layer))
-            return self.neural_network[-1][0]
+        if self.neural_network is None:
+            raise ValueError('You need neural network!')
+        if len(input_values) != len(self.neural_network[0][0]):
+            raise ValueError('Number of inputs must match with number of input neurons!')
+        input_values = np.array(input_values)
+        self.neural_network[0][0] = activation(input_values + self.neural_network[0][1])
+        for i in range(0, len(self.neural_network) - 1):
+            instant_layer = []
+            for j in range(0, len(self.neural_network[i][2])):
+                instant_layer.append(sum(self.neural_network[i][0]*self.neural_network[i][2][j]))
+            instant_layer += self.neural_network[i+1][1]
+            self.neural_network[i + 1][0] = activation(np.array(instant_layer))
+        return self.neural_network[-1][0]
 
 
 if __name__ == '__main__':
     neural_network = NeuralNetwork()
-    neural_network.run_neural_network([1])
-    neural_network.build_neural_network(3, 3, 3)
+    neural_network.build_neural_network(1, 1, 1)
     print(neural_network.run_neural_network([1]))
 
 
